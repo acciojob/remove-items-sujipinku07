c69d442665e46bf9b58e7bab95ce451a65b8a2d9
sujipinku07 <sujipinku07@gmail.com>
2024-09-29 16:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document
+        .getElementById("removeButton")
+        .addEventListener("click", function () {
+          const colorSelect = document.getElementById("colorSelect");
+          const selectedIndex = colorSelect.selectedIndex;
+
+          if (selectedIndex !== -1) {
+            colorSelect.remove(selectedIndex);
+          } else {
+            alert("Please select a color to remove.");
+          }
+        });
